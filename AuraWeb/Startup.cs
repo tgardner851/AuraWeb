c78e9bc6e6953cbe1b37550ce5e6bd4d730a51f4
Thomas Gardner <p0457@outlook.com>
2018-09-16 21:00:32 -0500

    Fix loading of assets by not using min (temp, fix)

--- AuraWeb/Startup.cs
+++ AuraWeb/Startup.cs
@@ -77,7 +77,14 @@ namespace AuraWeb
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = context =>
+                {
+                    context.Context.Response.Headers.Add("Cache-Control", "no-cache, no-store");
+                    context.Context.Response.Headers.Add("Expires", "-1");
+                }
+            });
             app.UseCookiePolicy();
 
             app.UseAuthentication();
