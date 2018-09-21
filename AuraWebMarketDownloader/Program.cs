09475e8ea26310447b28137fbb653a5d318d858c
Thomas Gardner <p0457@outlook.com>
2018-09-21 07:44:34 -0500

    Dont sleep on the last cycle, also update timer

--- AuraWebMarketDownloader/Program.cs
+++ AuraWebMarketDownloader/Program.cs
@@ -112,8 +112,8 @@ DELETE FROM RegionMarketOrders
         {
             int SECONDS_TIMEOUT = 120;
 
-            int SECONDS_BETWEEN_ACTIONS = 15;
-            int SECONDS_BETWEEN_REGIONS = 30;
+            int SECONDS_BETWEEN_ACTIONS = 5;
+            int SECONDS_BETWEEN_REGIONS = 5;
             int MS_BETWEEN_ACTIONS = SECONDS_BETWEEN_ACTIONS * 1000;
             int MS_BETWEEN_REGIONS = SECONDS_BETWEEN_REGIONS * 1000;
 
@@ -145,7 +145,7 @@ VALUES (@RegionId, @OrderId, @TypeId, @SystemId, @LocationId,
                 Console.WriteLine(String.Format("[REGION IDS] Found {0} Regions to process.", regionIds.Count));
                 #endregion
 
-                for (int x = 0; x < regionIds.Count; x++) // Loop through the regions
+                for (int x = 0; x <= regionIds.Count; x++) // Loop through the regions
                 {
                     int regionId = regionIds[x];
                     Console.WriteLine(String.Format("[REGION {0}] Processing Region Id {0} ({1} of {2})...", regionId, x + 1, regionIds.Count));
@@ -276,9 +276,12 @@ VALUES (@RegionId, @OrderId, @TypeId, @SystemId, @LocationId,
                     double percentComplete = ((x + 1) / regionIds.Count) * 100;
                     Console.WriteLine(String.Format("[REGION {0}] Finished Processing Region Id {0} ({1}%)", regionId, percentComplete.ToString("##.##")));
 
-                    // Give the servers a break
-                    Console.WriteLine(String.Format("[SLEEP] Sleeping for {0} seconds...zzzz", SECONDS_BETWEEN_REGIONS.ToString()));
-                    Thread.Sleep(MS_BETWEEN_REGIONS);
+                    // Give the servers a break, this time only if not the last one
+                    if (x < regionIds.Count)
+                    {
+                        Console.WriteLine(String.Format("[SLEEP] Sleeping for {0} seconds...zzzz", SECONDS_BETWEEN_REGIONS.ToString()));
+                        Thread.Sleep(MS_BETWEEN_REGIONS);
+                    }
                 }
             }
             catch (Exception e)
