534b6cb58ca22695eecbb56d6721cde9d04dda7b
Thomas Gardner <p0457@outlook.com>
2018-10-14 19:18:09 -0500

    Styled character page

--- AuraWeb/Controllers/CharacterController.cs
+++ AuraWeb/Controllers/CharacterController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
