4e4a0672130d10045bddaa318de4c198da9b9df6
Thomas Gardner <p0457@outlook.com>
2018-10-27 18:31:46 -0500

    Add log watch scripts

---
+++ watch-log-dev.bat
@@ -0,0 +1,2 @@
+cd C:\remote\My-Scripts\powershell\
+Powershell.exe -File ./watch-log-auraweb-dev.ps1
---
+++ watch-log-prod.bat
@@ -0,0 +1,2 @@
+cd C:\remote\My-Scripts\powershell\
+Powershell.exe -File ./watch-log-auraweb-prod.ps1
